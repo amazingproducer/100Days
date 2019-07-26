b62f8cdb9214f0462f040fc825e3f096553fe790
amazingproducer <amazingconsumer@gmail.com>
2019-07-26 17:26:51 -0400

    Day 4 - Fancy Conditional Sums complete

--- 004.fancy.conditional.sums/fancy_conditional_sums.py
+++ 004.fancy.conditional.sums/fancy_conditional_sums.py
@@ -2,3 +2,21 @@
 # get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+class FancyConditionalSum:
+    def example():
+        sum = 0
+        for i in range(1,10):
+            if i % 5 == 0:
+                sum += i
+            elif i % 3 == 0:
+                sum += i
+        return sum
+
+    def solve():
+        sum = 0
+        for i in range(1, 1000):
+            if i % 5 == 0:
+                sum += i
+            elif i % 3 == 0:
+                sum += i
+        return sum
