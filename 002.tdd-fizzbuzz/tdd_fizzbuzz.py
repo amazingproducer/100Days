67d520820460a2955a3e221d1d8e32788e02170b
amazingproducer <amazingconsumer@gmail.com>
2019-07-26 05:17:07 -0400

    Day 3 - w00t! Fast Feedback Loopings

--- 002.tdd-fizzbuzz/tdd_fizzbuzz.py
+++ 002.tdd-fizzbuzz/tdd_fizzbuzz.py
@@ -4,6 +4,8 @@ class FizzBuzz:
         for i in range(upper_limit, lower_limit):
             if i % 15 == 0:
                 fb_list.append("FizzBuzz")
+            elif i % 5 == 0:
+                fb_list.append("Buzz")
             else:
                 fb_list.append(str(i))
         return fb_list
--- 002.tdd-fizzbuzz/test_tdd_fizzbuzz.py
+++ 002.tdd-fizzbuzz/test_tdd_fizzbuzz.py
@@ -3,8 +3,13 @@ from inspect import getargspec
 from tdd_fizzbuzz import FizzBuzz as fb
 
 class FizzBuzzSetup(unittest.TestCase):
-    def test_range_has_been_set(self):
+    def test_for_FizzBuzz_replacement(self):
         fb_list = fb.replace_numbers()
         self.assertEqual(fb_list[14], "FizzBuzz")
+
+    def test_for_Buzz_replacement(self):
+        fb_list = fb.replace_numbers()
+        self.assertEqual(fb_list[4], "Buzz")
+
 if __name__ == '__main__':
     unittest.main()
