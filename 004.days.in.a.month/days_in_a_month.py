85de34d9549060ca911cb633e1286cbc5af12c83
amazingproducer <amazingconsumer@gmail.com>
2019-07-26 20:54:49 -0400

    Day 4 - Days in a Month complete

---
+++ 004.days.in.a.month/days_in_a_month.py
@@ -0,0 +1,11 @@
+# Write a function that accepts a month as an integer between 1 and 12 which
+# outputs the number of days in that month. This function should also accept an
+# argument to return the value for leap years, which is false by default.
+
+def days_in_month(month: int, leap_year: bool = False) -> int:
+    days_by_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+    while 0 < month or  12 >= month:
+        if leap_year == True and month == 2:
+            return days_by_month[1] + 1
+        return days_by_month[(month - 1)]
+
