29ad7b5c9dc361dd4b00ff8ac49a01135c3d8c34
amazingproducer <amazingconsumer@gmail.com>
2019-08-04 07:18:44 -0400

    Day 12 - smallest multiple - WIP - I think I need to try a simpler problem already

--- 011.smallest.multiple/smallest_multiple.py
+++ 011.smallest.multiple/smallest_multiple.py
@@ -2,6 +2,8 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 # Let's solve it the hard way:
+# It would be crazy to ask our program to solve it this way. Is it cheating if
+# I look for a better algorithm to do this?
 
 # x  2  2  2  2  3  3  5  7  11 13 17 19
 # ======================================
@@ -29,8 +31,8 @@
 # This should give us 232792560
 
 class SmallestMultiple:
-    exercise_set = set(range(1,21))
-    def generate_nth_prime(n):
+    exercise_set = range(2,21) # I think using this makes it not work right
+    def generate_nth_prime(n): # I thought bringing this in would help
         j = [2]
         i = 3
         while len(j) < n:
@@ -42,13 +44,17 @@ class SmallestMultiple:
             i += 2
         return j[-1]
 
-    def get_from_set(a=exercise_set)):
-        column_list = a
-        for item in a:
-            i = 2
-            item_list = []
-            while i:
-                j = SmallestMultiple.generate_nth_prime(i)
-                if item % j == 0 and item != j:
-                    print("This isn't done yet.")
+    def get_from_set(): # I sure wish I documented what I meant to do here when I attempted this earlier.
+        i = 0
+        j = None
+        while not j:
+            i += 1
+            for item in range(20,1, -1): # Maybe because the range was backwards?
+                print(item, i)
+                if i % item != 0:
+                    break
+            else:
+                j = i
+        return j
 
+print(SmallestMultiple.get_from_set())
