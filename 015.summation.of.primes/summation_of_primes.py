44d1e713480eccd0c2da17034a1ee21419f2b90b
amazingproducer <amazingconsumer@gmail.com>
2019-08-12 07:42:42 -0400

    Day 20 - create next-prime generator, attempt to optimize #20 Summation of Primes

--- 015.summation.of.primes/summation_of_primes.py
+++ 015.summation.of.primes/summation_of_primes.py
@@ -1,45 +1,57 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
 
-# Let's borrow some code from an earlier prime number exercise!
-
 class PrimeToys:
-    # TODO: apply optimized primal functions to this class
-    # TODO: create generator: generate_next_prime(n)
-    # Accepts n (the n-th prime number) as starting value
-    # as input and yields a list containing all primes up to the nth.
-    # calling next() on it will yield a list containing all primes
-    # up to the nth+1 prime, etc
-    def generate_nth_prime(n): # Borrowed from Day 7
+    # Borrowed from day 19
+    def is_prime(n):
+        if n <= 1:
+            return False
+        elif n <= 2:
+            return True
+        elif n % 2 == 0:
+            return False
+        else:
+            for i in range(3, int(n**.5) + 1, 2):
+                if n % i == 0:
+                    return False
+            return True
+
+    def get_nth_prime(n):
         j = [2]
         i = 3
         while len(j) < n:
-            for k in j: # because naming stuff is hard
-                if i % k == 0:
-                    break
-            else:
+            if PrimeToys.is_prime(i):
                 j.append(i)
-            i += 2 # because incrementing by one would include even numbers. Wasteful!
-        return j[-1]
+            i += 2
+        return j, j[-1]
 
-    def sum_prime_ceiling(ceiling=10):
+    def generate():
+        j = [2]
+        i = 3
+        while i:
+            if PrimeToys.is_prime(i):
+                j.append(i)
+                yield [j, j[-1]]
+            i += 2
+
+    def get_sum(ceiling=10):
+        p = PrimeToys.generate()
         n = 0
-        i = 1
-        q = 0
+        r = 0
         while n < ceiling:
-            q += PrimeToys.generate_nth_prime(i)
-            i += 1
-            n = PrimeToys.generate_nth_prime(i)
-        return q
+            q = next(p)
+            if q[-1] < ceiling:
+                n = q[-1]
+                r = sum(q[0])
+            else:
+                return r
 
 
-# Does my n-th generator work?
-#print(PrimeToys.generate_nth_prime(0)) # 2 ugly
-#print(PrimeToys.generate_nth_prime(1)) # 2 that's why it's ugly
-#print(PrimeToys.generate_nth_prime(2)) # 3 works, but ugly
-
-# Does my solution solve the example correctly?
-
-#print(PrimeToys.sum_prime_ceiling()) # Seems to work; prints 17
-
-print(PrimeToys.sum_prime_ceiling(2000000))
+print("Testing example (expecting 17):", str(PrimeToys.get_sum()))
+import datetime
+start_time = datetime.datetime.utcnow()
+print("Solution start time: " + str(start_time))
+print("Attempting solution:", str(PrimeToys.get_sum(2000000)))
+finish_time = datetime.datetime.utcnow()
+print("Finish time: " + str(finish_time))
+print("Elapsed time:  "  + str(finish_time - start_time))
