19481e68067904d9108b6085a2668abfd5264293
amazingproducer <amazingconsumer@gmail.com>
2019-09-19 05:15:01 -0400

    Day 58 - add doctests for #19

--- exercises/015.10001st.prime/10001st_prime.py
+++ exercises/015.10001st.prime/10001st_prime.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
 # that the 6th prime is 13.
 # What is the 10001st prime number?
@@ -8,6 +9,11 @@
 
 class Prime:
     def generate_nth_prime(n):
+        """Return the n-th prime number when given an integer n.
+
+        >>> Prime.generate_nth_prime(6)
+        13
+        """
         j = [2]
         i = 3
         while len(j) < n:
@@ -19,6 +25,10 @@ class Prime:
             i += 2  # because incrementing by one would include even numbers. Wasteful!
         return j[-1]
 
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
 
 print(f"Example solution (we expect 13): {Prime.generate_nth_prime(6)}")
 print(f"Exercise solution: {Prime.generate_nth_prime(10001)}")
