0eda6df4a74cb6953bb83bf1d438a8857d0bf41f
amazingproducer <amazingconsumer@gmail.com>
2019-10-03 06:03:06 -0400

    Day 72 - PrimeTools API - amend flask static routes again

--- products/024.primetools.web.api/flask_api.py
+++ products/024.primetools.web.api/flask_api.py
@@ -8,7 +8,7 @@ import requests
 deploy_port = int(os.environ.get('PORT', 5000))
 callback_url = str(os.environ.get('PT_DEV_CALLBACK'))
 
-api = Flask(__name__, static_url_path='/docs', static_folder='html')
+api = Flask(__name__, static_url_path='', static_folder='html')
 
 help_text = """
 <h3>PrimeTools Web API:</h3>
@@ -59,7 +59,7 @@ def help():
     """Print the ugly, manually-written route list."""
     return help_text
 
-@api.route('/docs/<string:path>')
+@api.route('/<path:path>')
 def send_docs(path):
     return send_from_directory('html', path)
 
