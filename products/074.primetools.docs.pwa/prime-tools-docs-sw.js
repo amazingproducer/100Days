d71b12f2c8e70be939c51c268bdf83b77d3feed5
amazingproducer <amazingconsumer@gmail.com>
2019-10-05 10:47:36 -0400

    Day 74 - PrimeTools PWA - simplify service worker

--- products/074.primetools.docs.pwa/prime-tools-docs-sw.js
+++ products/074.primetools.docs.pwa/prime-tools-docs-sw.js
@@ -62,32 +62,3 @@ function updateCache(request, response) {
 
 
 
-// INSTALL THE THING
-let deferredPrompt = null;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-});
-
-async function install() {
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-    console.log(deferredPrompt)
-    deferredPrompt.userChoice.then(function(choiceResult){
-
-      if (choiceResult.outcome === 'accepted') {
-      console.log('PrimeTools Docs has been installed.');
-    } else {
-      console.log('PrimeTools Docs installation prompt ignored.');
-    }
-
-    deferredPrompt = null;
-
-    });
-
-
-  }
-}
