943dc4ca86645a1bda9e0f5534a0c55592b1cad9
amazingproducer <amazingconsumer@gmail.com>
2019-10-27 18:23:53 -0400

    Day 97 - LongJacket - WIP - investigate chrome notification errors

--- products/076.longjacket.pwa/push_server.py
+++ products/076.longjacket.pwa/push_server.py
@@ -96,9 +96,9 @@ def notify():
     app.logger.info(f"pushing with VAPID claims: {vapid_claim_string}")
     for item in items:
         try:
-            app.logger.info(type(json.loads(item.subscription_info)),
-                json.loads(item.subscription_info.replace("\'","\"")),
-                json.loads(item.subscription_info.replace("'",'"')))
+#            app.logger.info(type(json.loads(item.subscription_info)),
+#                json.loads(item.subscription_info.replace("\'","\"")),
+#                json.loads(item.subscription_info.replace("'",'"')))
             if "'" in item.subscription_info:
                 subscription_method = json.loads(item.subscription_info.replace("\'","\""))
             else:
