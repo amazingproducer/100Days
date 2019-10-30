852a700a5291d63e4e77a39e7ecd9ee11fd24091
amazingproducer <amazingconsumer@gmail.com>
2019-10-30 03:19:56 -0400

    Day 99 - LongJacket - investigate JSON quote handling issues

--- products/076.longjacket.pwa/push_server.py
+++ products/076.longjacket.pwa/push_server.py
@@ -137,7 +137,7 @@ def notify():
             webpush(
 #                subscription_info=subscription_method,
 #                subscription_info=json.loads(item.subscription_info),
-                subscription_info=jsonify(item.subscription_info),
+                subscription_info=json.loads(str(item.subscription_info)),
                 data="Investigate sea monster at: lat:19.759 lng:-154.9845",
                 vapid_private_key=WEBPUSH_VAPID_PRIVATE_KEY,
                 vapid_claims=vapid_claim
